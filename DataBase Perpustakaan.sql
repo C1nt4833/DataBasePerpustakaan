bac547f2d07952a2be3011b03522f4fb16d91b84
C1nt4833 <cintawardana87@gmail.com>
2025-07-04 07:29:39 +0700

    Add files via upload

---
+++ DataBase Perpustakaan.sql
@@ -0,0 +1,69 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.4.3 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.8.0.6908
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Dumping structure for table perpustakaan_db.anggota
+CREATE TABLE IF NOT EXISTS `anggota` (
+  `id_anggota` int NOT NULL AUTO_INCREMENT,
+  `nama` varchar(100) DEFAULT NULL,
+  `alamat` text,
+  `no_hp` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id_anggota`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table perpustakaan_db.anggota: ~2 rows (approximately)
+INSERT INTO `anggota` (`id_anggota`, `nama`, `alamat`, `no_hp`) VALUES
+	(1, 'Rina', 'Jl.Mawar no.10', '089999999999'),
+	(2, 'Budi', 'Jl.Melati No.5', '08234567891');
+
+-- Dumping structure for table perpustakaan_db.buku
+CREATE TABLE IF NOT EXISTS `buku` (
+  `id_buku` int NOT NULL AUTO_INCREMENT,
+  `judul` varchar(100) DEFAULT NULL,
+  `penerbut` varchar(100) DEFAULT NULL,
+  `tahun_terbit` year DEFAULT NULL,
+  `stok` int DEFAULT NULL,
+  PRIMARY KEY (`id_buku`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table perpustakaan_db.buku: ~2 rows (approximately)
+INSERT INTO `buku` (`id_buku`, `judul`, `penerbut`, `tahun_terbit`, `stok`) VALUES
+	(1, 'Laskar Pelangi', 'Bentang Pustaka', '2005', 10),
+	(2, 'Bumi', 'Gramedia', '2014', 5);
+
+-- Dumping structure for table perpustakaan_db.peminjaman
+CREATE TABLE IF NOT EXISTS `peminjaman` (
+  `id_peminjaman` int NOT NULL AUTO_INCREMENT,
+  `id_buku` int DEFAULT NULL,
+  `id_anggota` int DEFAULT NULL,
+  `tanggal_pinjam` date DEFAULT NULL,
+  `tanggal_kembali` date DEFAULT NULL,
+  PRIMARY KEY (`id_peminjaman`),
+  KEY `id_buku` (`id_buku`),
+  KEY `id_anggota` (`id_anggota`),
+  CONSTRAINT `peminjaman_ibfk_1` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`),
+  CONSTRAINT `peminjaman_ibfk_2` FOREIGN KEY (`id_anggota`) REFERENCES `anggota` (`id_anggota`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table perpustakaan_db.peminjaman: ~2 rows (approximately)
+INSERT INTO `peminjaman` (`id_peminjaman`, `id_buku`, `id_anggota`, `tanggal_pinjam`, `tanggal_kembali`) VALUES
+	(1, 1, 1, '2025-07-01', '2025-07-08'),
+	(2, 2, 2, '2025-07-02', '2025-07-09');
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
